ff2a3434147bd7c2cb93ea7fefc5ed7fb4a2e084
Brian <bstep1@umbc.edu>
2022-09-23 10:30:00 -0400

    add navbar drawer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import NavbarDrawer from './NavbarDrawer';
 
 export default function Navbar() {
 
     return (
         <AppBar position='sticky'>
             <Toolbar>
+                <NavbarDrawer />
                 <Typography variant='h5'>
                     Iron Journal
                 </Typography>
---
+++ components/NavbarDrawer.tsx
@@ -0,0 +1,24 @@
+import { Menu } from '@mui/icons-material'
+import { Drawer, IconButton, List, ListItem } from '@mui/material'
+import { useState } from 'react'
+
+export default function NavbarDrawer() {
+    const [open, setOpen] = useState(false)
+    const toggleOpen = () => setOpen(!open)
+
+    return (
+        <>
+            <IconButton onClick={toggleOpen} aria-label='open navbar menu drawer'>
+                <Menu />
+            </IconButton>
+
+            <Drawer anchor='left' open={open} onClose={toggleOpen}>
+                <List sx={{ px: 2 }}>
+                    <ListItem>Item 1</ListItem>
+                    <ListItem>Item 2</ListItem>
+                    <ListItem>Item 3</ListItem>
+                </List>
+            </Drawer>
+        </>
+    )
+}
