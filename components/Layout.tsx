809eeb8c9bd91c09dbc581f052fa2320e71fd1ce
Brian <bstep1@umbc.edu>
2022-09-23 10:51:10 -0400

    add color theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,18 @@
-import { Container } from '@mui/material';
-import { ReactNode } from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { Container, createTheme } from '@mui/material';
+import { ReactNode, useState } from 'react';
+import { bluePalette, greenPalette } from '../styles/themePalettes';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const [palette, setPalette] = useState(bluePalette)
+    const theme = createTheme({
+        palette: { ...palette }
+    })
+
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <Navbar />
             <main>
                 <Container>
@@ -13,6 +20,6 @@ export default function Layout({ children }: { children: ReactNode }) {
                 </Container>
             </main>
             <Footer />
-        </>
+        </ThemeProvider>
     )
 }
---
+++ styles/themePalettes.ts
@@ -0,0 +1,17 @@
+export const bluePalette = {
+    primary: {
+        main: '#2196f3'
+    },
+    secondary: {
+        main: '#ffa726'
+    }
+}
+
+export const greenPalette = {
+    primary: {
+        main: '#66bb6a'
+    },
+    secondary: {
+        main: '#ba68c8'
+    }
+}
