4b0c151f128a35d705e7f3931822226c58c6ab9d
Brian <bstep1@umbc.edu>
2022-10-06 14:59:26 -0400

    test out sql api on exercises endpoint

--- lib/backend/postgresService.ts
+++ lib/backend/postgresService.ts
@@ -3,11 +3,20 @@ import { Pool } from 'pg'
 //pool handles managing the client connections
 const pool = new Pool()
 
-export async function test() {
+export async function fetchAllExercises() {
     try {
-        const res = await pool.query('select * from exercise')
+        const res = await pool.query('select exercise_name, is_active, comments from exercise ')
         return res.rows
     } catch (e) {
-        console.log(e)
+        console.error(e)
+    }
+}
+
+export async function fetchActiveExercises() {
+    try {
+        const res = await pool.query('select exercise_name, comments from exercise where is_active = true')
+        return res.rows
+    } catch (e) {
+        console.error(e)
     }
 }
--- pages/api/exercises.ts
+++ pages/api/exercises.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fetchCollection } from '../../lib/backend/mongoService'
+import { fetchActiveExercises, fetchAllExercises } from '../../lib/backend/postgresService'
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
-        const exercises = await fetchCollection('exercises')
+        // const exercises = await fetchCollection('exercises')
+        const exercises = await fetchAllExercises()
         res.status(200).json(exercises)
     } catch (e) {
         res.status(500).json({ error: 'error fetching exercises' })
