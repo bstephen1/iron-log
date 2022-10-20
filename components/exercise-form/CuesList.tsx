d2a7bf14b576d7b92e1cc5257627f34c15bcccb5
Brian <bstep1@umbc.edu>
2022-10-20 14:23:07 -0400

    integrate add button into list

--- components/exercise-form/CuesList.tsx
+++ components/exercise-form/CuesList.tsx
@@ -1,10 +1,11 @@
+import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
-import { Button, Divider, IconButton, InputBase, Paper, Stack } from '@mui/material';
+import { Divider, Grow, IconButton, InputBase, Paper, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useExerciseFormContext } from './useExerciseForm';
 
 export default function CuesList() {
-    const { dirtyExercise, cleanExercise, setField } = useExerciseFormContext()
+    const { cleanExercise, dirtyExercise, setField } = useExerciseFormContext()
     const cues = dirtyExercise?.cues ?? []
 
     function handleDeleteCue(i: number) {
@@ -24,34 +25,96 @@ export default function CuesList() {
                 Cues
             </Divider>
             {/* todo: Component for each ListItem. drag n drop? */}
-            <Button
-                disabled={!cleanExercise}
-                onClick={() => setField('cues', ['', ...cues])}
-            >
-                Add
-            </Button>
             <Stack spacing={2}>
                 {cues.map((cue, i) => (
-                    <CueInput
+                    <CueInputBase
                         key={i}
                         index={i}
                         value={cue}
                         handleDelete={handleDeleteCue}
                         handleUpdate={handleUpdateCue}
                     />))}
+                <CueInputAdd
+                    watcher={cleanExercise?.name}
+                    handleAdd={(newCue: string) => {
+                        console.log(newCue)
+                        setField('cues', [...cues, newCue])
+                    }}
+                />
             </Stack>
         </>
     )
 }
 
+interface AddProps {
+    watcher: any,
+    handleAdd: (newCue: string) => void,
+}
+function CueInputAdd(props: AddProps) {
+    const { watcher, handleAdd } = props
+    const [value, setValue] = useState('')
 
-interface Props {
+    const handleClear = () => setValue('')
+    function handleAddAndClear() {
+        handleClear()
+        handleAdd(value)
+    }
+
+    useEffect(() => {
+        handleClear()
+    }, [watcher])
+
+    return (
+        <Paper
+            component='form'
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+        >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder='Add cue'
+                autoFocus={!value}
+                multiline
+                value={value}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        (document.activeElement as HTMLElement).blur()
+                        handleAddAndClear()
+                    }
+                }}
+                onChange={(e) => setValue(e.target.value)}
+                inputProps={{ 'aria-label': 'Add cue' }}
+            />
+            <Grow in={!!value}>
+                <IconButton
+                    type='button'
+                    sx={{ p: '10px' }}
+                    disabled={!value}
+                    aria-label='add cue'
+                    onClick={handleAddAndClear}
+                >
+                    <CheckIcon />
+                </IconButton>
+            </Grow>
+            <IconButton
+                type='button'
+                sx={{ p: '10px' }}
+                disabled={!value}
+                aria-label='clear cue'
+                onClick={handleClear}
+            >
+                <ClearIcon />
+            </IconButton>
+        </Paper>
+    )
+}
+
+interface BaseProps {
     index: number,
     value: string,
     handleDelete: (key: any) => void,
     handleUpdate: (value: string, index: number) => void,
 }
-function CueInput(props: Props) {
+function CueInputBase(props: BaseProps) {
     const { index, handleDelete, handleUpdate } = props
     const [value, setValue] = useState(props.value)
 
