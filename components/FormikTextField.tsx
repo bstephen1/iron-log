848187f31bfa80593704c40f90aa06d908712b83
Brian <bstep1@umbc.edu>
2022-10-26 09:41:37 -0400

    reference commit with onChange and full input props examples

--- components/FormikTextField.tsx
+++ components/FormikTextField.tsx
@@ -1,19 +1,35 @@
-// @ts-nocheck
 import { TextField } from '@mui/material'
-import { useField } from 'formik'
+import { FieldHookConfig, useField } from 'formik'
+import { InputHTMLAttributes } from 'react'
 
-export default function FormikTextField({ label, ...props }) {
-  const [field, meta, helpers] = useField(props)
+interface Props {
+  label: string
+  defaultHelperText?: string
+}
+// The type is now quite robust, but it's probably not actually needed.
+// The only thing being passed to fieldProps is type and name, and mui's TextField
+// already sets type as text. I'll commit it with the full type for reference, but
+// I'm going to try cutting it down. If only the fields being used are explicitly defined
+// it should be more clear as well, rather than this nebulous "fieldProps" that can be
+// almost anything.
+export default function FormikTextField({
+  label,
+  defaultHelperText = ' ',
+  ...fieldProps
+}: Props & InputHTMLAttributes<HTMLInputElement> & FieldHookConfig<string>) {
+  const [field, meta, helpers] = useField(fieldProps)
+
+  console.log(fieldProps)
 
   return (
     <TextField
       label={label}
       value={meta.value}
       error={!!meta.error}
-      helperText={meta.error ?? ' '}
+      helperText={meta.error ?? defaultHelperText}
       // if doing onChange have to use the helper; or can pass field.onChange directly to inputProps
       // onChange={e => helpers.setValue(e.target.value)}
-      inputProps={{ ...field, ...props }}
+      inputProps={{ ...field, ...fieldProps }}
     />
   )
 }
