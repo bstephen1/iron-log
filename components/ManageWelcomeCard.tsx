abbd76f543bff7ac71fc8100ebb28b90052cbf9c
Brian <bstep1@umbc.edu>
2022-11-03 07:06:18 -0400

    remove Card

--- components/ManageWelcomeCard.tsx
+++ components/ManageWelcomeCard.tsx
@@ -1,20 +1,16 @@
-import { Card, CardContent, Stack, Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 
 export default function ManageWelcomeCard() {
   return (
-    <Card>
-      <CardContent sx={{ textAlign: 'center' }}>
-        <Stack spacing={2}>
-          <Typography variant="h5">Manage Exercises</Typography>
-          <Typography variant="body2">
-            Select an exercise in the dropdown to edit its values. Or, type in a
-            new exercise to create it.
-          </Typography>
-          <Typography variant="body2">
-            Any edits will be saved automatically.
-          </Typography>
-        </Stack>
-      </CardContent>
-    </Card>
+    <Stack spacing={2} sx={{ textAlign: 'center' }}>
+      <Typography variant="h5">Manage Exercises</Typography>
+      <Typography variant="body2">
+        Select an exercise in the dropdown to edit its values. Or, type in a new
+        exercise to create it.
+      </Typography>
+      <Typography variant="body2">
+        Any edits will be saved automatically.
+      </Typography>
+    </Stack>
   )
 }
