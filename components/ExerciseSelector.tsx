28720297d931510480b4583ba2230b7dec3057f5
Brian <bstep1@umbc.edu>
2022-11-05 17:30:09 -0400

    fix filtering on type

--- components/ExerciseSelector.tsx
+++ components/ExerciseSelector.tsx
@@ -1,12 +1,15 @@
 // @ts-nocheck
 // todo: typing
-import { Autocomplete } from '@mui/material'
+import { Autocomplete, createFilterOptions } from '@mui/material'
 import { useState } from 'react'
 import { addExercise, useCategories } from '../lib/frontend/restService'
 import Exercise from '../models/Exercise'
 import { ExerciseStatusOrder } from '../models/ExerciseStatus'
 import CategoryFilter from './CategoryFilter'
 import { withAsync } from './withAsync'
+
+// this allows the autocomplete to filter options based on what the user is typing
+const filter = createFilterOptions<Exercise | NewExerciseStub>()
 
 function ExerciseSelectorBase({
   exercise,
@@ -57,17 +60,21 @@ function ExerciseSelectorBase({
       getOptionLabel={(option) => option.name}
       // was going to pull this out to a separate function but the param type definitions are long and annoying
       filterOptions={(options, params) => {
-        const filtered = options.filter((option) => {
-          console.log(option.categories)
-          return (
-            !categoryFilter ||
-            option.categories.some(
-              (category) => category === categoryFilter.name
-            )
-          )
-        })
-
         const { inputValue } = params
+        let filtered = filter(options, params)
+
+        if (categoryFilter) {
+          filtered = filtered.filter((option) => {
+            return (
+              // todo: null out category if selecting something that's not in the category?
+              // todo: on clicking category chip in form, setCategory to that value?
+              option.name === inputValue || // if you filter out an exercise you can still type it in manually
+              option.categories.some(
+                (category) => category === categoryFilter.name
+              )
+            )
+          })
+        }
 
         // append an option to add the current input
         const isExisting = options.some((option) => inputValue === option.name)
