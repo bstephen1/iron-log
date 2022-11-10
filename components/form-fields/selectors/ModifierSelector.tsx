09e80be9e7c17c1c76ab6c0f80a4d0e157f207c9
Brian <bstep1@umbc.edu>
2022-11-10 16:48:07 -0500

    add typing, following withAsync pattern

--- components/form-fields/selectors/ModifierSelector.tsx
+++ components/form-fields/selectors/ModifierSelector.tsx
@@ -1,29 +1,49 @@
+import {
+  ComponentPropsWithoutRef,
+  ComponentType,
+  Dispatch,
+  SetStateAction,
+} from 'react'
+import { KeyedMutator } from 'swr'
 import { addModifier } from '../../../lib/frontend/restService'
 import Modifier from '../../../models/Modifier'
 import { NamedStub } from '../../../models/NamedObject'
 import withAsync from '../withAsync'
 import SelectorBase from './SelectorBase'
 
-const withModifier = (Component) => (props) => {
-  class NewModifierStub implements NamedStub {
-    constructor(public name: string, public status = 'Add New') {}
-  }
-
-  return (
-    <Component
-      {...props}
-      options={props.modifiers}
-      label="Modifier"
-      // todo: do we want modifiers to have status?
-      groupBy={(option: Modifier | NewModifierStub) =>
-        option.status === 'Add New' ? option.status : 'Modifier'
-      }
-      placeholder="Select or Add a Modifier"
-      StubConstructor={NewModifierStub}
-      Constructor={Modifier}
-      addNewItem={addModifier}
-    />
-  )
+interface WithModifierProps {
+  modifiers?: Modifier[]
+  modifier: Modifier | null
+  mutate: KeyedMutator<Modifier>
+  handleChange: Dispatch<SetStateAction<Modifier | null>>
 }
+const withModifier =
+  <T extends ComponentType<any>>(Component: T) =>
+  ({
+    modifiers,
+    modifier,
+    ...props
+  }: WithModifierProps & ComponentPropsWithoutRef<T>) => {
+    class NewModifierStub implements NamedStub {
+      constructor(public name: string, public status = 'Add New') {}
+    }
+
+    return (
+      <Component
+        {...(props as any)}
+        options={modifiers}
+        value={modifier}
+        label="Modifier"
+        // todo: do we want modifiers to have status?
+        groupBy={(option: Modifier | NewModifierStub) =>
+          option.status === 'Add New' ? option.status : 'Modifier'
+        }
+        placeholder="Select or Add a Modifier"
+        StubConstructor={NewModifierStub}
+        Constructor={Modifier}
+        addNewItem={addModifier}
+      />
+    )
+  }
 
 export const ModifierSelector = withModifier(withAsync(SelectorBase<Modifier>))
