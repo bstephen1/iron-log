bded816aca6707215af68ea4d165f1d603890b2f
Brian <bstep1@umbc.edu>
2022-11-10 07:46:08 -0500

    tidy up

--- components/form-fields/selectors/SelectorBase.tsx
+++ components/form-fields/selectors/SelectorBase.tsx
@@ -3,43 +3,47 @@ import { useMemo } from 'react'
 import { KeyedMutator } from 'swr'
 import { GenericAutocompleteProps } from '../../../lib/util'
 import Category from '../../../models/Category'
-import { NamedObjectKeyless } from '../../../models/NamedObject'
+import NamedObject, { NamedObjectKeyless } from '../../../models/NamedObject'
 
-interface SelectorBaseProps<T> extends GenericAutocompleteProps<T> {
+interface SelectorBaseProps<T, S> extends GenericAutocompleteProps<T | S> {
   handleChange: (value: T | null) => void
   categoryFilter: Category | null
   mutate: KeyedMutator<T[]>
-  StubConstructor: new (name: string) => T // todo: this should be keyless
+  StubConstructor: new (name: string) => S
   Constructor: new (name: string) => T
   addNewItem: (value: T) => void
+  // have to explicitly declare options is T[] or Autocomplete will think it's (T | S)[].
+  // options is the db res, so will not include the keyless Stub value
+  options: T[]
 }
 // this component is intended to be ingested as the base layer of a HOC.
 export default function SelectorBase<
-  T extends NamedObjectKeyless // todo: what should it extend?
+  T extends NamedObject,
+  S extends NamedObjectKeyless
 >({
   // value,
   handleChange,
-  // options, // todo: these should be part of autocompleteProps
+  options, // todo: these should be part of autocompleteProps
   categoryFilter,
   mutate,
   StubConstructor,
   Constructor,
   addNewItem,
   ...autocompleteProps
-}: SelectorBaseProps<T>) {
+}: SelectorBaseProps<T, S>) {
   // This allows the autocomplete to filter options as the user types, in real time.
   // It needs to be the result of this function call, and we can't call it
   // outside the component while keeping the generic. So, useMemo to cache the result
-  const filter = useMemo(() => createFilterOptions<T>(), [])
+  const filter = useMemo(() => createFilterOptions<T | S>(), [])
 
   return (
-    <Autocomplete<T>
+    <Autocomplete<T | S>
       openOnFocus
       selectOnFocus
       clearOnBlur
       handleHomeEndKeys
       autoHighlight // todo: this sometimes pops up over Category selector for Exercises
-      // options={options}
+      options={options}
       // value={value} // todo: this is just undefined currently. Probably will hash out when typing
       isOptionEqualToValue={(a, b) => a.name === b.name}
       getOptionLabel={(option) => option.name}
@@ -47,7 +51,7 @@ export default function SelectorBase<
         // add the new item if selected
         if (option && !('_id' in option)) {
           const newItem = new Constructor(option.name)
-          mutate(autocompleteProps.options?.concat(newItem))
+          mutate(options?.concat(newItem))
           addNewItem(newItem)
           handleChange(newItem)
         } else {
