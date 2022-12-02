8e7187535ebc70c504816f69651c0a8ab5f88fb5
Brian <bstep1@umbc.edu>
2022-12-02 08:05:26 -0500

    remove old field

--- models/Record.ts
+++ models/Record.ts
@@ -1,13 +1,11 @@
 import { generateId } from '../lib/util'
 import Exercise from './Exercise'
 import Set from './Set'
-import { SetType } from './SetType'
 
 export default class Record {
   constructor(
     public date: string,
     public exercise: Exercise | null = null,
-    public type: SetType = SetType.STANDARD,
     public activeModifiers: string[] = [],
     public fields: (keyof Set)[] = ['weight', 'reps', 'effort'],
     public sets: Set[] = [],
