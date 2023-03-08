646574a53e6c62c24eb72d6a80eff04327f86cb5
Brian <bstep1@umbc.edu>
2023-03-08 11:24:35 -0500

    add a sessions page redirect to today

---
+++ pages/sessions/index.tsx
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { DATE_FORMAT } from '../../lib/frontend/constants'
+
+export default function Page() {
+  const router = useRouter()
+
+  // need to wrap router.push in a useEffect because otherwise it would try to
+  // render serverside and cause an error since the router doesn't exist yet.
+  useEffect(() => {
+    const today = dayjs()
+    router.push(`sessions/${today.format(DATE_FORMAT)}`)
+  }, [router])
+
+  return <></>
+}
