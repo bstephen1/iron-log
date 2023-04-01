2f4310c51db3b34375d709577d24af64c3806a20
Brian <bstep1@umbc.edu>
2023-04-01 17:35:56 -0400

    patch app bar to make it less sensitive
    also undo previous linter change, which broke the precommit cycle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,19 @@ import NavbarDrawer from './NavbarDrawer'
 
 export default function Navbar() {
   function HideOnScroll({ children }: { children: ReactElement }) {
-    const trigger = useScrollTrigger()
+    // todo: This scroll trigger is way too sensitive and there's no way to fix that.
+    // disableHysteresis means it will trigger at the Y value given in threshold.
+    // So you can only make it hide at a certain Y value.
+    // Without disableHysteresis it will also immediately unhide if the scroll Y value
+    // increases AT ALL. Which means if you slightly tap the screen on mobile, or anything
+    // gets added to the dom (thus increasing its length), the app bar pops back up.
+    // This is just a stopgap solution currently. Ideally the trigger should have separate
+    // "down" and "up" thresholds, which are both relative values rather than the absolute
+    // Y position.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 250,
+    })
 
     return (
       <Slide appear={false} direction="down" in={!trigger}>
