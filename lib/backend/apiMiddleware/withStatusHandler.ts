b8d5d00b2761622b89f09d653f27e04d93765dbc
Brian <bstep1@umbc.edu>
2023-04-04 11:33:06 -0400

    change logging message format

--- lib/backend/apiMiddleware/withStatusHandler.ts
+++ lib/backend/apiMiddleware/withStatusHandler.ts
@@ -11,7 +11,7 @@ export default function withStatusHandler<T>(handler: ApiHandler<T>) {
     try {
       const userId = await getUserId(req, res)
       if (process.env.SERVER_LOG_LEVEL === 'verbose') {
-        console.log(`Incoming ${req.method} on ${req.url} for user ${userId}`)
+        console.log(`${req.method} ${req.url} for user ${userId}`)
       }
 
       const payload = await handler(req, userId)
