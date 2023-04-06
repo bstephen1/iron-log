925ddc11a23297110ca6a226e5e15c992e6e4fe6
Brian <bstep1@umbc.edu>
2023-04-06 11:07:11 -0400

    remove outer component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,41 +1,33 @@
 import { AppBar, Box, Slide, Toolbar, Typography } from '@mui/material'
 import Link from 'next/link'
-import { ReactElement, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import LoginButton from './LoginButton'
 import NavbarDrawer from './NavbarDrawer'
 
 export default function Navbar() {
-  function HideOnScroll({ children }: { children: ReactElement }) {
-    const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(true)
 
-    // This is a simple static Y value. May want to expand it such that
-    // when scrolling down y1 relative pixels it hides, and when scrolling up y2
-    // relative pixels it unhides.
-    const handleScroll = () => {
-      setIsVisible(window.scrollY < 250)
-    }
-
-    // mui has a "useScrollTrigger" hook that it recommends using for this situation,
-    // but said hook has horrendous performance on firefox, and even produces a console warning.
-    useEffect(() => {
-      // adding "passive" is supposed to increase performance for scrolling. See:
-      // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
-      window.addEventListener('scroll', handleScroll, { passive: true })
-
-      return () => {
-        window.removeEventListener('scroll', handleScroll)
-      }
-    }, [])
-
-    return (
-      <Slide appear={false} direction="down" in={isVisible}>
-        {children}
-      </Slide>
-    )
+  // This is a simple static Y value. May want to expand it such that
+  // when scrolling down y1 relative pixels it hides, and when scrolling up y2
+  // relative pixels it unhides.
+  const handleScroll = () => {
+    setIsVisible(window.scrollY < 250)
   }
 
+  // mui has a "useScrollTrigger" hook that it recommends using for this situation,
+  // but said hook has horrendous performance on firefox, and even produces a console warning.
+  useEffect(() => {
+    // adding "passive" is supposed to increase performance for scrolling. See:
+    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <HideOnScroll>
+    <Slide appear={false} direction="down" in={isVisible}>
       <AppBar position="sticky" sx={{ mb: 2 }}>
         <Toolbar>
           <NavbarDrawer />
@@ -46,6 +38,6 @@ export default function Navbar() {
           <LoginButton />
         </Toolbar>
       </AppBar>
-    </HideOnScroll>
+    </Slide>
   )
 }
