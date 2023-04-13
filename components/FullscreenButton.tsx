9948400500e7f5ca88ab6da57be729830d597fa8
Brian <bstep1@umbc.edu>
2023-04-12 20:48:40 -0400

    fix undefined document error

--- components/FullscreenButton.tsx
+++ components/FullscreenButton.tsx
@@ -5,22 +5,29 @@ import { useState } from 'react'
 
 export default function FullscreenButton() {
   const [isFullscreen, setIsFullscreen] = useState(false)
-  const elem = document.querySelector('body')
+
+  let elem: HTMLBodyElement | null = null
+
+  // document doesn't exist in initial server render so need to guard against it
+  if (typeof window !== 'undefined') {
+    elem = document.querySelector('body')
+
+    // Components don't re-render when document changes, so have to add an event listener.
+    // User could exit fullscreen via eg keyboard, not just the button, so this prevents
+    // the button from getting out of sync.
+    addEventListener('fullscreenchange', () => {
+      setIsFullscreen(!!document.fullscreenElement)
+    })
+  }
 
   const toggleFullscreen = async () => {
     if (isFullscreen) {
+      // this would cause SSR to error if isFullscreen didn't init as false
       document.exitFullscreen()
     } else {
       await elem?.requestFullscreen()
     }
   }
-
-  // Components don't re-render when document changes, so have to add an event listener.
-  // User could exit fullscreen via eg keyboard, not just the button, so this prevents
-  // the button from getting out of sync.
-  addEventListener('fullscreenchange', () => {
-    setIsFullscreen(!!document.fullscreenElement)
-  })
 
   return (
     <Box px={1}>
