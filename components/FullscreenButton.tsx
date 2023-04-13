4809bf716c267d01302be92d55b37d2bf3477617
Brian <bstep1@umbc.edu>
2023-04-12 20:55:14 -0400

    change document query
    previous query was preventing scroll

--- components/FullscreenButton.tsx
+++ components/FullscreenButton.tsx
@@ -6,11 +6,11 @@ import { useState } from 'react'
 export default function FullscreenButton() {
   const [isFullscreen, setIsFullscreen] = useState(false)
 
-  let elem: HTMLBodyElement | null = null
+  let elem: HTMLElement | null = null
 
   // document doesn't exist in initial server render so need to guard against it
   if (typeof window !== 'undefined') {
-    elem = document.querySelector('body')
+    elem = document.documentElement
 
     // Components don't re-render when document changes, so have to add an event listener.
     // User could exit fullscreen via eg keyboard, not just the button, so this prevents
