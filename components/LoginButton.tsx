f5d305ff53d9a52536f426e2b33ea86baea8f83e
Brian <bstep1@umbc.edu>
2023-04-21 11:05:06 -0400

    clear cache on sign out
    previous commit revealed an issue where local storage
    can get conflicted with different user data
    when multiple users sign in on the same browser
    
    Note: removed preventDefault from login buttons after
    trying to add types to the event because
    the button click type said
    preventDefault didn't exist.
    Also it didn't seem to be doing anything.
    
    Also removed session from _app because that apparently
    only does anything if you uses getInitialProps or getServerSideProps.
    Including it in app meant it was always undefined and not doing anything.

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,15 +1,36 @@
 import { LoadingButton } from '@mui/lab'
 import { Box } from '@mui/material'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useSWRConfig } from 'swr'
 
 export default function LoginButton() {
-  const handleSignin = (e: any) => {
-    e.preventDefault()
+  const { cache } = useSWRConfig()
+
+  // Clearing cache on sign in/out ensures there isn't leftover data from local storage
+  // if the user switches to a different account
+
+  // todo: delete on sign in/out events? Currently it will only be cleared when clicking the sign out button.
+  // [...next-auth] has "events" (setup like "callbacks") for signIn/signOut
+  // which may be better since it's possible to sign in via api, not just this button.
+  // But you can't useSWRConfig to access the cache since that isn't a component.
+  // And you can't access localStorage because it's server side.
+
+  // Since the cache is a map, it technically has access to cache.clear(), but that
+  // isn't officially listed in the Cache type.
+  const clearCache = () => {
+    for (const key of cache.keys()) {
+      cache.delete(key)
+    }
+  }
+
+  // this should actually never appear because of next-auth's middleware redirect
+  const handleSignin = () => {
+    clearCache()
     signIn(undefined, { callbackUrl: '/' })
   }
 
-  const handleSignout = (e: any) => {
-    e.preventDefault()
+  const handleSignout = () => {
+    clearCache()
     signOut()
   }
 
--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,7 +2,6 @@ import Layout from 'components/Layout'
 import useRouterLoading from 'components/loading/useRouterLoading'
 import useSWRCacheProvider from 'components/useSWRCacheProvider'
 import { swrFetcher } from 'lib/util'
-import { server } from 'msw-mocks/server'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { createContext } from 'react'
@@ -12,23 +11,12 @@ import { SWRConfig } from 'swr'
 
 export const RouterLoadingContext = createContext(false)
 
-// Enabling this will allow mock rest endpoints in dev mode.
-// In the docs this is supposed to need a separate browser setup,
-// but this is working with the node server. I guess it's something
-// with how nextjs is structured.
-if (process.env.MSW === 'enabled') {
-  server.listen()
-}
-
-interface IronLogPageProps {
-  session?: any // todo: not sure how to import Session from next-auth
-}
-function IronLog({ Component, pageProps }: AppProps<IronLogPageProps>) {
+function IronLog({ Component, pageProps }: AppProps) {
   const isRouterLoading = useRouterLoading()
   const provider = useSWRCacheProvider()
 
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider>
       <SWRConfig value={{ fetcher: swrFetcher, provider }}>
         <Layout>
           <RouterLoadingContext.Provider value={isRouterLoading}>
