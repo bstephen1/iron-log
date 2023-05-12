b475031bf50f98f52886bc53e03e28e43db0d46c
Brian <bstep1@umbc.edu>
2023-05-12 10:21:46 -0400

    update test pattern

--- pages/api/bodyweight-history/index.test.ts
+++ pages/api/bodyweight-history/index.test.ts
@@ -1,36 +1,32 @@
 import {
+  addBodyweight,
+  fetchBodyweightHistory,
+  updateBodyweight,
+} from 'lib/backend/mongoService'
+import {
   expectApiErrorsOnInvalidMethod,
   expectApiRespondsWithData,
 } from 'lib/testUtils'
 import Bodyweight from 'models/Bodyweight'
+import { vi } from 'vitest'
 import handler from './index.api'
-
-var mockFetch: vi.mock
-var mockAdd: vi.mock
-var mockUpdate: vi.mock
-
-vi.mock('lib/backend/mongoService', () => ({
-  fetchBodyweightHistory: (mockFetch = vi.fn()),
-  addBodyweight: (mockAdd = vi.fn()),
-  updateBodyweight: (mockUpdate = vi.fn()),
-}))
 
 const data = new Bodyweight(50, 'official')
 
 it('fetches given bodyweight', async () => {
-  mockFetch.mockReturnValue([data])
+  vi.mocked(fetchBodyweightHistory).mockResolvedValue([data])
 
   await expectApiRespondsWithData({ data: [data], handler })
 })
 
 it('adds given bodyweight', async () => {
-  mockAdd.mockReturnValue(data)
+  vi.mocked(addBodyweight).mockResolvedValue(data)
 
   await expectApiRespondsWithData({ data, handler, method: 'POST' })
 })
 
 it('updates given bodyweight', async () => {
-  mockUpdate.mockReturnValue(data)
+  vi.mocked(updateBodyweight).mockResolvedValue(data)
 
   await expectApiRespondsWithData({ data, handler, method: 'PUT' })
 })
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -8,7 +8,13 @@ import { server } from './msw-mocks/server'
 // todo: does vitest have a different way of doing this?
 dotenv.config({ path: './.env.test' })
 
-vi.mock('lib/backend/mongoConnect', () => ({}))
+// vi.mock will import the actual module and automock all exports.
+// If a factory is provided, it replaces the actual module with the factory.
+vi.mock('lib/backend/mongoConnect', () => ({
+  db: { collection: vi.fn() },
+  clientPromise: vi.fn(),
+}))
+vi.mock('lib/backend/mongoService')
 vi.mock('pages/api/auth/[...nextauth].api', () => ({ authOptions: vi.fn() }))
 vi.mock('next-auth', () => ({
   getServerSession: () => ({ user: { id: '1234567890AB' } }),
