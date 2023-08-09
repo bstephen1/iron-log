8866c8bc7803a2e03dde3bd433b0b660a13203f1
Brian <bstep1@umbc.edu>
2023-08-09 08:23:08 -0400

    remove unused useRouterLoading hook

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,27 +1,20 @@
 import Layout from 'components/Layout'
-import useRouterLoading from 'components/loading/useRouterLoading'
 import useSWRCacheProvider from 'components/useSWRCacheProvider'
 import { swrFetcher } from 'lib/util'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import { createContext } from 'react'
 import 'styles/globals.css'
 import 'styles/nprogress.css'
 import { SWRConfig } from 'swr'
 
-export const RouterLoadingContext = createContext(false)
-
 function IronLog({ Component, pageProps }: AppProps) {
-  const isRouterLoading = useRouterLoading()
   const provider = useSWRCacheProvider()
 
   return (
     <SessionProvider>
       <SWRConfig value={{ fetcher: swrFetcher, provider }}>
         <Layout>
-          <RouterLoadingContext.Provider value={isRouterLoading}>
-            <Component {...pageProps} />
-          </RouterLoadingContext.Provider>
+          <Component {...pageProps} />
         </Layout>
       </SWRConfig>
     </SessionProvider>
