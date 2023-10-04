4eae81d36348a9302cb3a96fb19b8363110c4ada
Brian <bstep1@umbc.edu>
2023-10-04 11:55:54 -0400

    add removeEventListener

--- components/FullscreenButton.tsx
+++ components/FullscreenButton.tsx
@@ -1,33 +1,35 @@
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
 import { Box, IconButton } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function FullscreenButton() {
   const [isFullscreen, setIsFullscreen] = useState(false)
+  const elem = useRef<HTMLElement | null>(null)
 
-  let elem: HTMLElement | null = null
+  // Note: document doesn't exist in server, so make sure only client is accessing it
 
-  // document doesn't exist in initial server render so need to guard against it
-  if (typeof window !== 'undefined') {
-    elem = document.documentElement
+  // Note: pressing F11 is browser fullscreen, which is different than javascript fullscreen.
+  // So it doesn't register any fullscreen events. F11 works but won't update the button,
+  // and there's no way to detect it unless you watch for and intercept the F11 key.
 
-    // Components don't re-render when document changes, so have to add an event listener.
-    // User could exit fullscreen via eg keyboard, not just the button, so this prevents
-    // the button from getting out of sync.
-    addEventListener('fullscreenchange', () => {
+  useEffect(() => {
+    elem.current = document.documentElement
+    const updateFullscreenState = () => {
       setIsFullscreen(!!document.fullscreenElement)
-    })
-  }
-
-  const toggleFullscreen = async () => {
-    if (isFullscreen) {
-      // this would cause SSR to error if isFullscreen didn't init as false
-      document.exitFullscreen()
-    } else {
-      await elem?.requestFullscreen()
     }
-  }
+
+    // Components don't re-render when document changes, so have to add an event listener
+    // in case user exits via Esc key
+    window.addEventListener('fullscreenchange', updateFullscreenState)
+    return () =>
+      window.removeEventListener('fullscreenchange', updateFullscreenState)
+  }, [])
+
+  const toggleFullscreen = async () =>
+    isFullscreen
+      ? document?.exitFullscreen()
+      : await elem?.current?.requestFullscreen()
 
   return (
     <Box px={1}>
