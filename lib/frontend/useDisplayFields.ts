5e81f243825a70f515b02ae4593c327566182ed5
Brian <bstep1@umbc.edu>
2023-10-24 15:08:53 -0400

    change useDisplayFields to use exercise

--- lib/frontend/useDisplayFields.ts
+++ lib/frontend/useDisplayFields.ts
@@ -1,13 +1,13 @@
+import Exercise from 'models/AsyncSelectorOption/Exercise'
 import {
   DEFAULT_DISPLAY_FIELDS,
   DEFAULT_DISPLAY_FIELDS_SPLIT_WEIGHT,
 } from 'models/DisplayFields'
-import Record from 'models/Record'
 
-export default function useDisplayFields(record: Record) {
+export default function useDisplayFields(exercise?: Exercise | null) {
   return (
-    record?.exercise?.displayFields ??
-    (record?.exercise?.attributes?.bodyweight
+    exercise?.displayFields ??
+    (exercise?.attributes?.bodyweight
       ? DEFAULT_DISPLAY_FIELDS_SPLIT_WEIGHT
       : DEFAULT_DISPLAY_FIELDS)
   )
