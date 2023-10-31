bffa8ccab63039e499e4c386f08f397b9bae515b
Brian <bstep1@umbc.edu>
2023-10-31 08:57:39 -0400

    update no history text

--- components/session/history/HistoryCardsSwiper.tsx
+++ components/session/history/HistoryCardsSwiper.tsx
@@ -69,9 +69,13 @@ export default function HistoryCardsSwiper({ isQuickRender }: Props) {
   // assumes filter has end date set to the current record's date (so will exclude it)
   if (!records.length) {
     return (
-      <Typography textAlign="center" py={2}>
-        No history found for this exercise!
-      </Typography>
+      <>
+        <Typography textAlign="center" pb={2}>
+          No history found for this exercise!
+          <br />
+          History is determined by exercise name + modifiers + set type.
+        </Typography>
+      </>
     )
   }
 
