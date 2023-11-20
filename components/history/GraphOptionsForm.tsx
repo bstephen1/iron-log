e758e312b36aa90586a211c58bad0e26ba18d7e3
Brian <bstep1@umbc.edu>
2023-11-20 11:00:31 -0500

    extract graph options to component

---
+++ components/history/GraphOptionsForm.tsx
@@ -0,0 +1,103 @@
+import {
+  FormControlLabel,
+  FormGroup,
+  InputAdornment,
+  Switch,
+  TextField,
+} from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import RecordDisplay from 'components/history/RecordDisplay'
+import RecordDisplaySelect from 'components/history/RecordDisplaySelect'
+import { UpdateState } from 'lib/util'
+
+export interface GraphOptions {
+  showBodyweight?: boolean
+  includeUnofficial?: boolean
+  smoothLine?: boolean
+  clothingOffset: number
+}
+
+interface Props {
+  graphOptions: GraphOptions
+  updateGraphOptions: UpdateState<GraphOptions>
+  recordDisplay: RecordDisplay
+  updateRecordDisplay: UpdateState<RecordDisplay>
+}
+export default function GraphOptionsForm({
+  graphOptions,
+  updateGraphOptions,
+  recordDisplay,
+  updateRecordDisplay,
+}: Props) {
+  const { showBodyweight, includeUnofficial, smoothLine, clothingOffset } =
+    graphOptions
+
+  return (
+    <Grid container spacing={2}>
+      <Grid xs={8} alignItems="center" display="flex">
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={showBodyweight}
+                onChange={() =>
+                  updateGraphOptions({ showBodyweight: !showBodyweight })
+                }
+              />
+            }
+            label="Show bodyweight"
+          />
+          <FormControlLabel
+            disabled={!showBodyweight}
+            control={
+              <Switch
+                checked={includeUnofficial}
+                onChange={() =>
+                  updateGraphOptions({ includeUnofficial: !includeUnofficial })
+                }
+              />
+            }
+            label="Include unofficial weigh-ins"
+          />
+          <FormControlLabel
+            disabled={!showBodyweight}
+            control={
+              <Switch
+                checked={smoothLine}
+                onChange={() => updateGraphOptions({ smoothLine: !smoothLine })}
+              />
+            }
+            label="Use smoothed line"
+          />
+        </FormGroup>
+      </Grid>
+      <Grid xs={4}>
+        <TextField
+          type="number"
+          label="clothing offset"
+          autoComplete="off"
+          value={clothingOffset}
+          disabled={!showBodyweight || !includeUnofficial}
+          onChange={(e) =>
+            updateGraphOptions({
+              clothingOffset: isNaN(Number(e.target.value))
+                ? 0
+                : Number(e.target.value),
+            })
+          }
+          onWheel={(e) => e.target instanceof HTMLElement && e.target.blur()}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+          }}
+        />
+      </Grid>
+
+      <Grid xs={12}>
+        <RecordDisplaySelect
+          recordDisplay={recordDisplay}
+          updateRecordDisplay={updateRecordDisplay}
+        />
+      </Grid>
+    </Grid>
+  )
+}
--- components/history/HistoryGraph.tsx
+++ components/history/HistoryGraph.tsx
@@ -1,24 +1,9 @@
-import {
-  Box,
-  FormControlLabel,
-  FormGroup,
-  InputAdornment,
-  Switch,
-  TextField,
-  Typography,
-  useTheme,
-} from '@mui/material'
-import Grid from '@mui/material/Unstable_Grid2'
-import StyledDivider from 'components/StyledDivider'
-import QueryForm from 'components/history/QueryForm'
+import { Box, Stack, useTheme } from '@mui/material'
 import RecordDisplay from 'components/history/RecordDisplay'
-import RecordDisplaySelect from 'components/history/RecordDisplaySelect'
-import HistoryCardsSwiper from 'components/session/history/HistoryCardsSwiper'
 import dayjs from 'dayjs'
 import { DATE_FORMAT, DEFAULT_CLOTHING_OFFSET } from 'lib/frontend/constants'
 import { useBodyweightHistory, useRecords } from 'lib/frontend/restService'
 import useDesktopCheck from 'lib/frontend/useDesktopCheck'
-import Record from 'models/Record'
 import { Set } from 'models/Set'
 import { RecordHistoryQuery } from 'models/query-filters/RecordQuery'
 import { useCallback, useEffect, useMemo, useState } from 'react'
@@ -34,6 +19,8 @@ import {
   YAxis,
 } from 'recharts'
 import useResizeObserver from 'use-resize-observer'
+import GraphOptionsForm, { GraphOptions } from './GraphOptionsForm'
+import { UpdateState } from 'lib/util'
 
 interface GraphData {
   epochDate: number
@@ -71,17 +58,24 @@ interface Props {
 }
 export default function HistoryGraph({ query }: Props) {
   const { data: bodyweightData } = useBodyweightHistory()
-  const [showBodyweight, setShowBodyweight] = useState(true)
-  const [includeUnofficial, setIncludeUnofficial] = useState(false)
-  const [clothingOffset, setClothingOffset] = useState(DEFAULT_CLOTHING_OFFSET)
-  const [smoothLine, setSmoothLine] = useState(false)
-  const lineType = smoothLine ? 'basis' : 'monotone'
   const [recordDisplay, setRecordDisplay] = useState<RecordDisplay>({
     field: 'weight',
     operator: 'highest',
   })
+  const [graphOptions, setGraphOptions] = useState<GraphOptions>({
+    clothingOffset: DEFAULT_CLOTHING_OFFSET,
+    showBodyweight: true,
+  })
+  const { smoothLine, clothingOffset, showBodyweight, includeUnofficial } =
+    graphOptions
+  const lineType = smoothLine ? 'basis' : 'monotone'
   const { palette } = useTheme()
   const { records } = useRecords(query, !!query)
+
+  const updateRecordDisplay: UpdateState<RecordDisplay> = (changes) =>
+    setRecordDisplay((prev) => ({ ...prev, ...changes }))
+  const updateGraphOptions: UpdateState<GraphOptions> = (changes) =>
+    setGraphOptions((prev) => ({ ...prev, ...changes }))
 
   const isDesktop = useDesktopCheck()
 
@@ -177,167 +171,117 @@ export default function HistoryGraph({ query }: Props) {
   ])
 
   return (
-    <Grid container spacing={2}>
-      <Grid xs={8} alignItems="center" display="flex">
-        <FormGroup row>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={showBodyweight}
-                onChange={() => setShowBodyweight(!showBodyweight)}
-              />
-            }
-            label="Show bodyweight"
-          />
-          <FormControlLabel
-            disabled={!showBodyweight}
-            control={
-              <Switch
-                checked={includeUnofficial}
-                onChange={() => setIncludeUnofficial(!includeUnofficial)}
-              />
-            }
-            label="Include unofficial weigh-ins"
-          />
-          <FormControlLabel
-            disabled={!showBodyweight}
-            control={
-              <Switch
-                checked={smoothLine}
-                onChange={() => setSmoothLine(!smoothLine)}
-              />
-            }
-            label="Use smoothed line"
-          />
-        </FormGroup>
-      </Grid>
-      <Grid xs={4}>
-        <TextField
-          type="number"
-          label="clothing offset"
-          autoComplete="off"
-          value={clothingOffset}
-          disabled={!showBodyweight || !includeUnofficial}
-          onChange={(e) =>
-            setClothingOffset(
-              isNaN(Number(e.target.value)) ? 0 : Number(e.target.value)
-            )
-          }
-          onWheel={(e) => e.target instanceof HTMLElement && e.target.blur()}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-          }}
-        />
-      </Grid>
+    <Stack spacing={2}>
+      <GraphOptionsForm
+        {...{
+          graphOptions,
+          updateGraphOptions,
+          recordDisplay,
+          updateRecordDisplay,
+        }}
+      />
 
-      <Grid xs={12}>
-        <RecordDisplaySelect
-          recordDisplay={recordDisplay}
-          updateRecordDisplay={(changes) =>
-            setRecordDisplay((prev) => ({ ...prev, ...changes }))
-          }
-        />
-      </Grid>
-
-      <Grid xs={12} height="100%" flex="1 1 auto"></Grid>
-      <Grid container xs={12} justifyContent="center">
-        {/* apparently Grid can't accept a ref (even though it works...) so adding a Box to capture the container width */}
-        <Box ref={graphContainerRef} width="100%" height="100%">
-          {bodyweightData && (
-            // need to specify a height or the grid and container will both defer to each other and result in zero height
-            <ResponsiveContainer height={windowHeight}>
-              <LineChart
-                margin={{
-                  top: 20,
-                  right: 5,
-                  left: 15,
-                  bottom: 10,
-                }}
-                onClick={(s) => console.log(s)}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="epochDate"
-                  type="number"
-                  tickFormatter={(value) =>
-                    dayjs.unix(value).format(DATE_FORMAT)
-                  }
-                  domain={['auto', 'auto']}
-                  angle={-25}
-                  scale="time"
-                  textAnchor="end"
-                  tickMargin={10}
-                  height={80}
-                  // Prevent tooltip from not showing correct values when multiple lines exist.
-                  // This is apparently an open bug, because it should only affect an XAxis with type="category"
-                  // See: https://github.com/recharts/recharts/issues/3044#issuecomment-1322056012
-                  allowDuplicatedCategory={false}
-                />
-                <YAxis
+      <Box height="100%" flex="1 1 auto"></Box>
+      <Box
+        ref={graphContainerRef}
+        width="100%"
+        height="100%"
+        justifyContent="center"
+      >
+        {bodyweightData && (
+          // need to specify a height or the grid and container will both defer to each other and result in zero height
+          <ResponsiveContainer height={windowHeight}>
+            <LineChart
+              margin={{
+                top: 20,
+                right: 5,
+                left: 15,
+                bottom: 10,
+              }}
+              onClick={(s) => console.log(s)}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="epochDate"
+                type="number"
+                tickFormatter={(value) => dayjs.unix(value).format(DATE_FORMAT)}
+                domain={['auto', 'auto']}
+                angle={-25}
+                scale="time"
+                textAnchor="end"
+                tickMargin={10}
+                height={80}
+                // Prevent tooltip from not showing correct values when multiple lines exist.
+                // This is apparently an open bug, because it should only affect an XAxis with type="category"
+                // See: https://github.com/recharts/recharts/issues/3044#issuecomment-1322056012
+                allowDuplicatedCategory={false}
+              />
+              <YAxis
+                yAxisId="bodyweight"
+                name="weight"
+                dataKey="value"
+                type="number"
+                unit=" kg"
+                orientation="right"
+                stroke="green"
+                domain={['auto', 'auto']}
+              />
+              {showBodyweight && (
+                <Line
                   yAxisId="bodyweight"
-                  name="weight"
+                  name="bodyweight"
                   dataKey="value"
-                  type="number"
-                  unit=" kg"
-                  orientation="right"
+                  type={lineType}
                   stroke="green"
-                  domain={['auto', 'auto']}
+                  data={bodyweightGraphData}
                 />
-                {showBodyweight && (
-                  <Line
-                    yAxisId="bodyweight"
-                    name="bodyweight"
-                    dataKey="value"
-                    type={lineType}
-                    stroke="green"
-                    data={bodyweightGraphData}
-                  />
-                )}
-                <YAxis
-                  yAxisId="exercise"
-                  name="weight"
-                  dataKey="value"
-                  type="number"
-                  unit=" kg"
-                  orientation="left"
-                  // line color
-                  stroke={palette.primary.dark}
-                  domain={['auto', 'auto']}
-                />
+              )}
+              <YAxis
+                yAxisId="exercise"
+                name="weight"
+                dataKey="value"
+                type="number"
+                unit=" kg"
+                orientation="left"
+                // line color
+                stroke={palette.primary.dark}
+                domain={['auto', 'auto']}
+              />
+              {query?.exercise && (
                 <Line
                   yAxisId="exercise"
-                  name={query?.exercise}
+                  name={query.exercise}
                   dataKey="value"
                   stroke={palette.primary.dark}
                   type={lineType}
                   data={recordGraphData}
                 />
-                {/* todo: possible to show weigh-in type in tooltip? */}
-                <Tooltip
-                  trigger={isDesktop ? 'hover' : 'click'}
-                  labelFormatter={convertEpochToDate}
-                  formatter={(value, _, { payload }) =>
-                    `${value} kg${
-                      payload.type === 'unofficial' ? ' (unofficial)' : ''
-                    }`
-                  }
-                />
-                {/* todo: only show if multiple lines */}
-                <Legend verticalAlign="top" height={30} />
-                <Brush
-                  dataKey="epochDate"
-                  tickFormatter={convertEpochToDate}
-                  // these doesn't accept percentages...
-                  width={graphContainerWidth * 0.6}
-                  // could only eyeball this trying to get it centered.
-                  x={graphContainerWidth * 0.2}
-                  startIndex={getStartIndex(bodyweightGraphData)}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          )}
-        </Box>
-      </Grid>
-    </Grid>
+              )}
+              {/* todo: possible to show weigh-in type in tooltip? */}
+              <Tooltip
+                trigger={isDesktop ? 'hover' : 'click'}
+                labelFormatter={convertEpochToDate}
+                formatter={(value, _, { payload }) =>
+                  `${value} kg${
+                    payload.type === 'unofficial' ? ' (unofficial)' : ''
+                  }`
+                }
+              />
+              {/* todo: only show if multiple lines */}
+              <Legend verticalAlign="top" height={30} />
+              <Brush
+                dataKey="epochDate"
+                tickFormatter={convertEpochToDate}
+                // these doesn't accept percentages...
+                width={graphContainerWidth * 0.6}
+                // could only eyeball this trying to get it centered.
+                x={graphContainerWidth * 0.2}
+                startIndex={getStartIndex(bodyweightGraphData)}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
+      </Box>
+    </Stack>
   )
 }
