65f4ee0cf0440c053757be0a886242caca8f928b
Brian <bstep1@umbc.edu>
2024-09-19 14:52:50 -0400

    memoize tabs

---
+++ models/TabValue.ts
@@ -0,0 +1,12 @@
+import { parseAsStringEnum, useQueryState } from 'next-usequerystate'
+
+export type TabValue = (typeof tabValues)[number]
+export const tabValues = ['exercises', 'modifiers', 'categories'] as const
+
+/** small wrapper for useQueryState with the value "tab" to retrieve the tab with proper enum typing */
+export function useQueryTab() {
+  return useQueryState(
+    'tab',
+    parseAsStringEnum<TabValue>([...tabValues]).withDefault('exercises'),
+  )
+}
