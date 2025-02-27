aab3f33fa8a5e6343a33f1747d2702c666a9eb61
Brian <bstep1@umbc.edu>
2025-02-27 14:03:11 -0500

    fix undefined input object causing crash

--- components/form-fields/InputField.tsx
+++ components/form-fields/InputField.tsx
@@ -77,7 +77,12 @@ export default function InputField(props: Props & TextFieldProps) {
                 </Tooltip>
               </TransitionIconButton>
               {/* mui does not provide proper typing to fields on slotProps */}
-              {(textFieldProps.slotProps?.input as InputProps).endAdornment}
+              {
+                (
+                  (textFieldProps.slotProps?.input as InputProps | undefined) ??
+                  {}
+                ).endAdornment
+              }
             </>
           ),
         },
