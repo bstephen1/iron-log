9371874b390d40d68d5395e0bbdb6ba8935ff5f0
Brian <bstep1@umbc.edu>
2025-03-19 15:23:52 -0400

    add schemas for other types beside record and exercise
    record and exercise require complex conversions due to nested types, esp Units / Sets.
    Not sure if it's worth continuing with the conversion.

--- models/AsyncSelectorOption/Category.ts
+++ models/AsyncSelectorOption/Category.ts
@@ -7,7 +7,7 @@ import { Status } from '../Status'
 
 export interface Category extends AsyncSelectorOption {}
 
-export const categorySchema = asyncSelectorOptionSchema
+export const categorySchema = asyncSelectorOptionSchema.strict()
 
 export const createCategory = (name: string): Category =>
   createAsyncSelectorOption(name, Status.active)
