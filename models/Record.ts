269e0738639ef7b234aba6a2dfc851647652624a
Brian <bstep1@umbc.edu>
2025-03-20 11:39:54 -0400

    update Record and Set to have proper schemas

--- models/Record.ts
+++ models/Record.ts
@@ -1,21 +1,11 @@
 import { z } from 'zod'
 import { generateId } from '../lib/util'
-import { Exercise, exerciseSchema } from './AsyncSelectorOption/Exercise'
-import { Note, noteSchema } from './Note'
-import { DEFAULT_SET_TYPE, Set, setOperators, SetType } from './Set'
+import { exerciseSchema } from './AsyncSelectorOption/Exercise'
+import { noteSchema } from './Note'
+import { DEFAULT_SET_TYPE, setSchema, setTypeSchema } from './Set'
 
 // todo: add activeCategory (for programming)
-export interface Record {
-  _id: string
-  date: string
-  exercise: Exercise | null
-  activeModifiers: string[]
-  category: string | null
-  notes: Note[]
-  setType: SetType
-  sets: Set[]
-}
-
+export interface Record extends z.infer<typeof recordSchema> {}
 export const recordSchema = z.object({
   _id: z.string(),
   date: z.string(),
@@ -23,21 +13,8 @@ export const recordSchema = z.object({
   activeModifiers: z.array(z.string()),
   category: z.string().nullable(),
   notes: z.array(noteSchema),
-  setType: z.object({
-    field: z.string(),
-    // field: z.string().refine((field) => Object.keys(Set).includes(field)),
-    operator: z.enum(setOperators),
-    value: z.number().optional(),
-    min: z.number().optional(),
-    max: z.number().optional(),
-  }),
-  sets: z.array(
-    z.object({
-      reps: z.number(),
-      weight: z.number().nullable(),
-      rest: z.number().nullable(),
-    })
-  ),
+  setType: setTypeSchema,
+  sets: z.array(setSchema),
 })
 
 export const createRecord = (
--- models/Set.ts
+++ models/Set.ts
@@ -1,4 +1,5 @@
-import { Units } from './Units'
+import { z } from 'zod'
+import { unitsSchema } from './Units'
 
 /*
 todo:
@@ -12,26 +13,20 @@ And for failure it can be anything > 10 (or < 0 for rir). I guess you can rate t
 */
 
 /** An exercise set. */
-export type Set = {
-  [field in keyof Omit<Units, 'side'>]?: number
-} & {
-  side?: 'L' | 'R' | '' | null
-}
-
-/** Marks the structure of the set in a record. Reads as <operator> <value|range> <field>.
- *  Eg, "exactly 5 reps" means every set in the record is supposed to have 5 reps.
- *
- * Records with the same SetType are grouped together when pulling history so progress can be tracked.
- */
-export interface SetType {
-  field: keyof Set
-  operator: SetOperator
-  value?: number
-  /** used for "between" operator */
-  min?: number
-  /** used for "between" operator */
-  max?: number
-}
+export interface Set extends z.infer<typeof setSchema> {}
+// tricky to define this schema since we need to change the value types to number,
+// plus overwrite "side"
+const dimensions = unitsSchema.keyof().exclude(['side']).options
+export const setSchema = z
+  .strictObject(
+    dimensions.reduce(
+      (prev, key) => ({ ...prev, [key]: z.number() }),
+      // the key definition is the same idea as making a type from a const array
+      {} as { [key in (typeof dimensions)[number]]: z.ZodNumber }
+    )
+  )
+  .partial()
+  .extend({ side: z.enum(['L', 'R', '']).nullish() })
 
 export type SetOperator = (typeof setOperators)[number]
 export const setOperators = [
@@ -42,6 +37,22 @@ export const setOperators = [
   'total',
   'rest',
 ] as const
+
+/** Marks the structure of the set in a record. Reads as <operator> <value|range> <field>.
+ *  Eg, "exactly 5 reps" means every set in the record is supposed to have 5 reps.
+ *
+ * Records with the same SetType are grouped together when pulling history so progress can be tracked.
+ */
+export interface SetType extends z.infer<typeof setTypeSchema> {}
+export const setTypeSchema = z.strictObject({
+  field: unitsSchema.keyof(),
+  operator: z.enum(setOperators),
+  value: z.number().optional(),
+  /** used for "between" operator */
+  min: z.number().optional(),
+  /** used for "between" operator */
+  max: z.number().optional(),
+})
 
 export const DEFAULT_SET_TYPE: Readonly<SetType> = {
   operator: 'exactly',
