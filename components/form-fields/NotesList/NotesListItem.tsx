cb9d0dcb4ffd0260c57e0718129567e53258846f
Brian <bstep1@umbc.edu>
2025-03-23 11:11:39 -0400

    add remaining tests

--- components/form-fields/NotesList/NotesListItem.tsx
+++ components/form-fields/NotesList/NotesListItem.tsx
@@ -19,7 +19,7 @@ interface Props {
 export default function NotesListItem(props: Props) {
   const {
     note,
-    placeholder = '',
+    placeholder = 'Empty note (will be deleted)',
     index,
     handleDelete,
     handleUpdate,
