a4deafe262f73c284a3ec9c47cccaad9ea20e394
Brian <bstep1@umbc.edu>
2025-03-24 12:28:31 -0400

    add tests

---
+++ components/form-fields/actions/ActionItems.test.tsx
@@ -0,0 +1,57 @@
+import { ComponentProps } from 'react'
+import { render, screen, within } from '../../../lib/testUtils'
+import ActionItems from './ActionItems'
+
+const mockHandleDelete = vi.fn()
+const mockHandleDuplicate = vi.fn()
+
+const defaultProps: ComponentProps<typeof ActionItems> = {
+  id: 'id',
+  name: 'name',
+  type: 'category',
+  handleDelete: mockHandleDelete,
+  handleDuplicate: mockHandleDuplicate,
+}
+
+it('calls handlers when buttons are clicked', async () => {
+  const { user } = render(<ActionItems {...defaultProps} />)
+
+  await user.click(screen.getByRole('button', { name: /duplicate/i }))
+  expect(mockHandleDuplicate).toHaveBeenCalledWith(defaultProps.id)
+
+  await user.click(screen.getByRole('button', { name: /delete/i }))
+  // confirmation popup
+  await user.click(
+    within(screen.getByRole('dialog')).getByRole('button', {
+      name: /delete/i,
+    })
+  )
+  expect(mockHandleDelete).toHaveBeenCalledWith(defaultProps.id)
+
+  // usage only is visible for type "exercise"
+  expect(screen.queryByText(/usage/i)).not.toBeInTheDocument()
+})
+
+it('renders usage button', () => {
+  render(<ActionItems {...defaultProps} type="exercise" deleteDisabled />)
+
+  expect(screen.getByRole('button', { name: /usage/i })).toBeVisible()
+  expect(screen.getByRole('button', { name: /delete/i })).toBeDisabled()
+})
+
+it('does not render buttons if handlers are not provided', () => {
+  render(
+    <ActionItems
+      {...defaultProps}
+      handleDelete={undefined}
+      handleDuplicate={undefined}
+    />
+  )
+
+  expect(
+    screen.queryByRole('button', { name: /duplicate/i })
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', { name: /delete/i })
+  ).not.toBeInTheDocument()
+})
---
+++ components/form-fields/actions/UsageButton.test.tsx
@@ -0,0 +1,43 @@
+import { URI_RECORDS } from '../../../lib/frontend/constants'
+import { render, screen, useServer, waitFor } from '../../../lib/testUtils'
+import { createRecord } from '../../../models/Record'
+import UsageButton from './UsageButton'
+
+it('displays usage when clicked', async () => {
+  const date = '2020-02-02'
+  const record = createRecord(date, {
+    sets: [{ reps: 6 }, { reps: 6 }],
+    setType: { field: 'reps', operator: 'exactly', value: 6 },
+  })
+  const singularRecord = createRecord('2000-02-02', {
+    sets: [{ reps: 6 }],
+    setType: { field: 'reps', operator: 'exactly', value: 6 },
+  })
+  useServer(URI_RECORDS, [record, singularRecord])
+  const { user } = render(<UsageButton name="name" />)
+
+  // have to wait for server res
+  await user.click(await screen.findByLabelText('used in 2 records'))
+
+  // dialog
+  expect(screen.getByText(/usage for/i)).toBeVisible()
+  expect(screen.getByText(date)).toBeVisible()
+  expect(screen.getByText(/2 sets /i)).toBeVisible()
+  // singular set (extra trailing space ensures it's not just picking up the
+  // start of "sets")
+  expect(screen.getByText(/1 set /i)).toBeVisible()
+
+  await user.click(screen.getByText(/close/i))
+  await waitFor(() => {
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+  })
+})
+
+it('shows singular label', async () => {
+  const date = '2020-02-02'
+  const record = createRecord(date)
+  useServer(URI_RECORDS, [record])
+  render(<UsageButton name="name" />)
+
+  expect(await screen.findByLabelText('used in 1 record')).toBeVisible()
+})
