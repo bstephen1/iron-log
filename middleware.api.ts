85ed1722567e76bca6ba9928582155fc5e139294
Brian <bstep1@umbc.edu>
2025-03-25 09:00:34 -0400

    prevent nextauth from redirecting api calls to sign in page
    nextauth is undergoing a major rebranding to authjs in v5, so may hold off on merging this until after upgrading and ironing out any changes

--- middleware.api.ts
+++ middleware.api.ts
@@ -1,4 +1,12 @@
-// this will route to the login screen if not logged in, from any route
-// todo: might want to exclude pages/api so it can send json back instead of redirecting to sign in page
+// this will route to the login screen if not logged in
 // see: https://next-auth.js.org/configuration/nextjs#middleware
 export { default } from 'next-auth/middleware'
+
+// config allows customizing which routes to redirect if not signed in
+export const config = {
+  matcher: [
+    // Ignore routes in the api folder (using regex negative lookahead).
+    // This allows api requests to return json.
+    '/((?!api/).*)',
+  ],
+}
