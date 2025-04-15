ceca6c808d9c0d4496823a69a5ba30a2439eadb4
Brian <bstep1@umbc.edu>
2025-04-15 10:33:09 -0400

    remove cypress
    long live playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,11 +20,11 @@ export default defineConfig({
       // statements: 31.5,
     },
     // Unlike jest, vitest has the option to not globally import describe/it etc keywords.
-    // This is desireable to avoid conflicts with cypress, which also globally defines those keywords.
+    // This is desireable to avoid conflicts with other testing libraries we may use (eg e2e tests).
     // However, disabling this causes two major issues with no easily traceable cause or solution:
     // - vscode can only sometimes detect vitest package to auto import keywords, so you have to manually type in imports
     // - tests start failing because apparently server.resetHandlers() stops being called before each test for unknown reasons
-    // In light of these issues we've opted to keep globals enabled and work around the cypress conflicts.
+    // In light of these issues we've opted to keep globals enabled.
     globals: true,
     // happy-dom? Supposed to be faster, but seems to not work well with mui components
     environment: 'jsdom',
