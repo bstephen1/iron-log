7e8ff08753c95c297db7d2dfa660efd710725fec
Brian <bstep1@umbc.edu>
2025-04-17 13:16:40 -0400

    allow rendering without an Alert

--- components/AppSnackbar.tsx
+++ components/AppSnackbar.tsx
@@ -1,17 +1,17 @@
-import { Alert, AlertColor, Snackbar } from '@mui/material'
+import { Alert, AlertColor, Button, Snackbar } from '@mui/material'
 import { CustomContentProps, closeSnackbar } from 'notistack'
 import { forwardRef } from 'react'
 
 export type AppSnackbarProps = {
-  /** defaults to success */
+  /** renders an alert of the given severity */
   severity?: AlertColor
+  action?: { text: string; onClick: () => void }
 }
-
 const AppSnackbar = forwardRef<
   HTMLDivElement,
   AppSnackbarProps & CustomContentProps
 >(function AppSnackbar(props, ref) {
-  const { message, severity, autoHideDuration, id } = props
+  const { message, severity, autoHideDuration, id, action, persist } = props
 
   // signals to the notistack wrapper to close the snackbar
   const handleClose = () => closeSnackbar(id)
@@ -22,7 +22,7 @@ const AppSnackbar = forwardRef<
       // notistack controls opening
       open
       // keep duration in sync with notistack's value
-      autoHideDuration={autoHideDuration}
+      autoHideDuration={persist ? null : autoHideDuration}
       // mui snackbar has three close triggers:
       // timeout, clickaway, escapeKeyDown (notistack only has timeout)
       onClose={handleClose}
@@ -30,15 +30,30 @@ const AppSnackbar = forwardRef<
         justifyContent: 'center',
         position: 'static',
       }}
+      // these props only display when the child Alert is not present
+      message={message}
+      action={
+        action ? (
+          <Button color="secondary" size="small" onClick={action.onClick}>
+            {action.text}
+          </Button>
+        ) : undefined
+      }
     >
-      <Alert
-        elevation={6}
-        severity={severity}
-        // adds a close button to the alert
-        onClose={handleClose}
-      >
-        {message}
-      </Alert>
+      {severity ? (
+        <Alert
+          elevation={6}
+          severity={severity}
+          // Adds a close button to the alert.
+          // Only necessary if the alert will not close by timeout
+          onClose={persist ? handleClose : undefined}
+          // ensure width is responsive -- snackbar automatically uses
+          // full screen width on small screens
+          sx={{ width: '100%' }}
+        >
+          {message}
+        </Alert>
+      ) : undefined}
     </Snackbar>
   )
 })
