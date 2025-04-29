bc7134db041bc0afa75a25856511bc08804dea73
Brian <bstep1@umbc.edu>
2025-04-29 10:04:53 -0400

    fix lint staged to only lint on staged files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,17 @@
-/** next lint requires us to pass each staged file name prefixed with "--file".
- * Normally lint-staged simply appends all file names to the end of the command you
- * provide, so we have to manually build the command to include the "--file" option.
- */
-const buildNextLintCommand = (filenames) =>
-  `npx next lint --fix --file ${filenames.join(' --file ')}`
+// lintstaged appends a list of all the staged files at the end of any commands
+// given to it, so the commands must support multiple files in a list like that.
+// Otherwise, a function can be passed instead of a string, which will have the
+// list of staged filenames as an arg.
 
 const prettier = 'npx prettier --write'
+// we must explicitly null out the dirs or it will still lint over all the dirs
+// specified in next.config.js
+const lint = 'npx next lint --fix --dir --file'
 
 // tasks are run in parallel, but they MUST have mutually exclusive matchers
 // to avoid multiple tasks writing to the same file
 export default {
-  '*.{ts,tsx}': [buildNextLintCommand, prettier],
+  '*.{ts,tsx}': [lint, prettier],
   // js files are usually jsut config for external pkgs, so we don't lint them
   '*.js': [prettier],
 }
