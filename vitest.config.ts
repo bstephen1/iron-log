d8a3b19461350927776635e1833fcf910a5f6511
Brian <bstep1@umbc.edu>
2025-05-16 20:57:33 -0400

    update config with CI settings
    - attempted to use dot reporter for CI but bizzarely it is not supported in CI.
    If you use it for CI it will silently revert to "default" reporter since
    CI terminal is not interactive (TTY)
    - attempted to turn off coverage cleanOnRerun but that just causes the test runner to crash when the coverage would have changed trying to read a missing file

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,18 +7,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     exclude: ['playwright', 'node_modules'],
-    coverage: {
-      // json reporters are needed for github action summary report
-      reporter: ['text-summary', 'html', 'json-summary', 'json'],
-      all: true,
-      enabled: true,
-      include: ['components', 'lib', 'pages', 'models'],
-      // thresholdAutoUpdate: true,
-      // branches: 77.48,
-      // functions: 45.13,
-      // lines: 31.5,
-      // statements: 31.5,
-    },
+    bail: process.env.CI ? 1 : 0,
     // Unlike jest, vitest has the option to not globally import describe/it etc keywords.
     // This is desireable to avoid conflicts with other testing libraries we may use (eg e2e tests).
     // However, disabling this causes two major issues with no easily traceable cause or solution:
@@ -26,13 +15,21 @@ export default defineConfig({
     // - tests start failing because apparently server.resetHandlers() stops being called before each test for unknown reasons
     // In light of these issues we've opted to keep globals enabled.
     globals: true,
-    // happy-dom? Supposed to be faster, but seems to not work well with mui components
     environment: 'jsdom',
-    // environmentMatchGlobs: [
-    //   ['*.tsx', 'jsdom'],
-    //   ['*.ts', 'node'],
-    // ],
     setupFiles: 'vitest.setup.ts',
+    // clear mock history, restore each implementation to its original, and restore original descriptors of spied-on objects
     restoreMocks: true,
+    coverage: {
+      // enable coverage
+      enabled: true,
+      // json reporters are needed for github action summary report
+      reporter: ['text-summary', 'html', 'json-summary', 'json'],
+      include: ['components', 'lib', 'pages', 'models'],
+      // thresholdAutoUpdate: true,
+      // branches: 77.48,
+      // functions: 45.13,
+      // lines: 31.5,
+      // statements: 31.5,
+    },
   },
 })
